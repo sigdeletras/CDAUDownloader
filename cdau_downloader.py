ae7f1e7defebfb3d225caa8cfea116980b972f98
Patricio Soriano <pasoriano@gmail.com>
2017-09-07 06:29:02 +0200

    renaming icon image

--- cdau_downloader.py
+++ cdau_downloader.py
@@ -202,7 +202,7 @@ class CDAUDownloader:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/CDAUDownloader/cdau_icon.png'
+        icon_path = ':/plugins/CDAUDownloader/icon.png'
         self.add_action(
             icon_path,
             text=self.tr(u'CDAU Downloader'),
